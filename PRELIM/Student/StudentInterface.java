0c1c3f44aef5291f64ae5418bbe5e69ffc03a3e8
GelaPostrero <143687976+GelaPostrero@users.noreply.github.com>
2023-10-25 20:50:48 +0800

    Add files via upload

---
+++ PRELIM/Student/StudentInterface.java
@@ -0,0 +1,33 @@
+package Student;
+
+import java.util.Scanner;
+
+class StudentInterface{
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      Student student = new Student();
+
+      System.out.print("Enter first name: ");
+      String firstName = sc.next();
+      student.setFirstName(firstName);
+
+      System.out.print("Enter middle name: ");
+      String middleName = sc.next();
+      student.setMiddleName(middleName);
+
+      System.out.print("Enter last name: ");
+      String lastName = sc.next();
+      student.setLastName(lastName);
+      
+      sc.nextLine();
+      
+      System.out.print("Enter suffix: ");
+      String suffix = sc.nextLine();
+      student.setSuffix(suffix);
+
+      System.out.println(student.getFullName()); 
+   }
+}
+
+
+ 
